afe462f8b76a0dacfed95d83bf5bd7ccebcfce3a
somotto <stephenonyango4783@gmail.com>
2025-06-23 17:41:16 +0300

    Add ApiCache class for caching API responses with TTL and max size

---
+++ src/api/cache.js
@@ -0,0 +1,38 @@
+class ApiCache {
+    constructor(maxSize = 100, ttl = 5 * 60 * 1000) {
+        // 5 minutes TTL
+        this.cache = new Map()
+        this.maxSize = maxSize
+        this.ttl = ttl
+    }
+
+    get(key) {
+        const item = this.cache.get(key)
+        if (!item) return null
+
+        if (Date.now() > item.expiry) {
+            this.cache.delete(key)
+            return null
+        }
+
+        return item.data
+    }
+
+    set(key, data) {
+        if (this.cache.size >= this.maxSize) {
+            const firstKey = this.cache.keys().next().value
+            this.cache.delete(firstKey)
+        }
+
+        this.cache.set(key, {
+            data,
+            expiry: Date.now() + this.ttl,
+        })
+    }
+
+    clear() {
+        this.cache.clear()
+    }
+}
+
+export const apiCache = new ApiCache()
